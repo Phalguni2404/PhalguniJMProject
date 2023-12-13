82864b7a87c0279a58dec66c4ec1cf1e589eef68
Phalguni <pallaphalguni2404@gmail.com>
2023-12-13 15:46:48 +0530

    Demo for team

---
+++ src/main/java/com/app/demo/PhalguniJMProject/App.java
@@ -0,0 +1,14 @@
+package com.app.demo.PhalguniJMProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Phalguni!" );
+        System.out.println("This is maven java project");
+    }
+}
